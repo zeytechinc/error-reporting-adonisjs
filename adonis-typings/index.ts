57b2c251f2e2da9a33abc1af07242ea0d1d2cb2c
Steve Krenek <steve.krenek@zeytech.com>
2022-07-12 16:24:52 -0500

    chore: added file headers to source for proper play-nice-ness

--- adonis-typings/index.ts
+++ adonis-typings/index.ts
@@ -1,3 +1,13 @@
+/*
+ * File: index.ts
+ * Created Date: Jul 12, 2022
+ * Copyright (c) 2022 Zeytech Inc. (https://zeytech.com)
+ * Author: Steve Krenek (https://github.com/skrenek)
+ * -----
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare module '@ioc:Adonis/Addons/Zeytech/ErrorReporting' {
   export interface DriverContract {
     name: string
--- src/Drivers/Logger/index.ts
+++ src/Drivers/Logger/index.ts
@@ -1,3 +1,13 @@
+/*
+ * File: index.ts
+ * Created Date: Jul 12, 2022
+ * Copyright (c) 2022 Zeytech Inc. (https://zeytech.com)
+ * Author: Steve Krenek (https://github.com/skrenek)
+ * -----
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import { DriverConfig, DriverContract } from '@ioc:Adonis/Addons/Zeytech/ErrorReporting'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { LoggerContract } from '@ioc:Adonis/Core/Logger'
